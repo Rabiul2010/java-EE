83312012510adb61fb14bd3b4f153f7c8d70c741
Rabiul Islam <Rabiul1020@gmail.com>
2016-07-21 15:42:07 +0600

    create an array list

---
+++ ArrayListExample.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ArrayListExample {
+	public static void main(String[] args) {
+		//create a lis
+		List<String> list= new ArrayList<String>();
+		System.out.println("intial size of list"+list.size());
+		
+	}
+
+}
