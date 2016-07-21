995dcbd5851293cce75b0637ce0fdef44962fc81
Rabiul Islam <Rabiul1020@gmail.com>
2016-07-21 15:49:46 +0600

    elements added to the array list

--- ArrayListExample.java
+++ ArrayListExample.java
@@ -6,7 +6,15 @@ public class ArrayListExample {
 	public static void main(String[] args) {
 		//create a lis
 		List<String> list= new ArrayList<String>();
-		System.out.println("intial size of list"+list.size());
+		System.out.println("intial size of list "+list.size());
+		//add elements to the array list
+		list.add("R");
+		list.add("A");
+		list.add("B");
+		list.add("I");
+		list.add("U");
+		list.add("L");
+		System.out.println("size of list after additions "+list.size());
 		
 	}
 
