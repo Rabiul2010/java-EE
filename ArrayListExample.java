d3671d81904c676d2edc75127f17395267a4c4e4
Rabiul Islam <Rabiul1020@gmail.com>
2016-07-21 16:24:56 +0600

    elements remove from list and check elements in list

--- ArrayListExample.java
+++ ArrayListExample.java
@@ -7,6 +7,7 @@ public class ArrayListExample {
 		//create a lis
 		List<String> list= new ArrayList<String>();
 		System.out.println("intial size of list "+list.size());
+		
 		//add elements to the array list
 		list.add("R");
 		list.add("A");
@@ -16,6 +17,20 @@ public class ArrayListExample {
 		list.add("L");
 		System.out.println("size of list after additions "+list.size());
 		
+		//display all elements
+		System.out.println("elements of list "+list);
+		
+		//display the first element of array list
+		System.out.println("first element of array list "+list.get(0));
+		
+		//remove elements from array list
+		list.remove(0);
+		System.out.println("size of list after deletions "+list.size());
+		System.out.println("content of list "+list);
+		
+		//search elements
+		System.out.println("Does list contain 'A' "+list.contains("A"));
+		System.out.println("Does list contain 'R' "+list.contains("R"));
 	}
 
 }
