f0c7c8c3f6f3c03e482d0afe1fbd1a72d2f055c6
Md. Rabiul Islam <rabiul1020@gmail.com>
2016-07-24 15:23:21 +0600

    Stack
    A stack is a data structure where elements are added to the top and also removed from top

--- src/rabiul/cu/cse/StackExample.java
+++ src/rabiul/cu/cse/StackExample.java
@@ -14,6 +14,7 @@ public class StackExample {
 		stack.push("Abdullah Al Mohammad Maruf");
 		stack.push("Swarup Muhuri");
 		stack.push("Nishan Das");
+		stack.push("Farhad Mohammad Shahriar");
 		/*
 		 * stack.push(1); stack.push(2); stack.push(3); stack.push(4);
 		 * stack.push(5);
