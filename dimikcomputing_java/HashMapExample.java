2375e5aaa4059e526939e178a32dce03069286fd
Md. Rabiul Islam <rabiul1020@gmail.com>
2016-07-24 10:43:48 +0600

    Size of map, values of map

--- dimikcomputing_java/HashMapExample.java
+++ dimikcomputing_java/HashMapExample.java
@@ -11,6 +11,12 @@ public class HashMapExample {
 		map.put("10205062","Mohammad Aminul Islam");
 		map.put("10205063", "Mohammad Rabiul Islam");
 		map.put("09205045","Abdullah Al Mohammad Maruf");
+		//print the size of map
+		System.out.println("Size of map after addition "+map.size());
+		//get elements
+		System.out.println("What is after "10205062":"+map.get("10205062"));
+		//get all the values
+		System.out.println("All values of map "+map.values());
 		
 	}
 
