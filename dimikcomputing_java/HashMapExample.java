711037f29cc1aa39bb4f872cf33fa566c9268771
Md. Rabiul Islam <rabiul1020@gmail.com>
2016-07-24 10:34:49 +0600

    put elements to the map

--- dimikcomputing_java/HashMapExample.java
+++ dimikcomputing_java/HashMapExample.java
@@ -5,8 +5,12 @@ import java.util.Map;
 
 public class HashMapExample {
 	public static void main(String[] args) {
-		//create a HashMap
+		//create a hash map
 		Map<String, String> map= new HashMap<String, String>();
+		//put elements to the map
+		map.put("10205062","Mohammad Aminul Islam");
+		map.put("10205063", "Mohammad Rabiul Islam");
+		map.put("09205045","Abdullah Al Mohammad Maruf");
 		
 	}
 
