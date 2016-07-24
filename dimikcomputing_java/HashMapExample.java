04eb506aabb50c731c8cdfd399cb31690f1f444b
Rabiul Islam <Rabiul1020@gmail.com>
2016-07-24 10:28:27 +0600

    create a HashMap

---
+++ dimikcomputing_java/HashMapExample.java
@@ -0,0 +1,13 @@
+package dimikcomputing_java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapExample {
+	public static void main(String[] args) {
+		//create a HashMap
+		Map<String, String> map= new HashMap<String, String>();
+		
+	}
+
+}
