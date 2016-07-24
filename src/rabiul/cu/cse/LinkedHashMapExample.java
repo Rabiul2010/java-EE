d60d3062c879199110c16ea04953346a5a65be77
Md. Rabiul Islam <rabiul1020@gmail.com>
2016-07-24 11:25:23 +0600

    LinkedHashMap
    LinkedHashMap maintain the elements insertion order

---
+++ src/rabiul/cu/cse/LinkedHashMapExample.java
@@ -0,0 +1,24 @@
+package rabiul.cu.cse;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LinkedHashMapExample {
+	//LinkedHashMap maintain the elements insertion order
+	public static void main(String[] args) {
+		//create a hash map
+		Map<String, String> map= new LinkedHashMap<String, String>();
+		//put elements to the map
+		map.put("10205062","Mohammad Aminul Islam");
+		map.put("10205063", "Mohammad Rabiul Islam");
+		map.put("09205045","Abdullah Al Mohammad Maruf");
+		//print the size of map
+		System.out.println("Size of map after addition "+map.size());
+		//get elements
+		System.out.println("value of '10205062' is: "+map.get("10205062"));
+		//get all the values
+		System.out.println("All values of map "+map.values());
+		
+	}
+
+}
